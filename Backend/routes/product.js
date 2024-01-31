bda2813555427947589835d13587ed4f382d5ac4
satish <satishkonduru0225@gmail.com>
2024-01-31 07:36:54 +0530

    Product API updated

--- Backend/routes/product.js
+++ Backend/routes/product.js
@@ -57,4 +57,59 @@ router.get('/getById/:id', auth.authenticateToken,(req, res)=>{
     } )
 })
 
+router.patch('/update',auth.authenticateToken,checkRole.checkRole ,(req, res)=>{
+    let product = req.body
+    var query = "update product set name=?, categoryId=?, description=?, price=? where id=?"
+    connection.query(query,[product.name, product.categogyId, product.description, product.price, product.id], (err, results)=>{
+        if(!err){
+            if(results.affectedRows == 0){
+                return res.status(404).json({message: 'Product Id does not found.'})
+            }
+            else{
+                return res.status(200).json({message: 'Product Updated Successfully'})
+            }
+        }
+        else{
+            return res.status(500).json(err)
+        }
+    })
+})
+
+
+router.delete('/delete/:id', auth.authenticateToken, checkRole.checkRole, (req, res) =>{
+    const id = req.params.id
+    var query = "delete from product where id=?"
+    connection.query(query,[id],(err, results)=>{
+        if(!err){
+            if(results.affectedRows == 0){
+                return res.status(404).json({message: 'Product Id dons not Found'})
+            }
+            else{
+                return res.status(200).json({message: 'Product Deleted Successfully'})
+            }
+        }
+        else{
+            return res.status(500).json(err)
+        }
+    } )
+})
+
+router.patch('/updateStatus', auth.authenticateToken, checkRole.checkRole, (req, res)=>{
+    let product = req.body
+    var query = "update product set status=? where id=?"
+    connection.query(query, [product.status, product.id], (err, results) => {
+        if(!err){
+            if(results.affectedRows == 0){
+                return res.status(404).json({message: 'Product Id does not Found'})
+            }
+            else{
+                return res.status(200).json({message: 'Product Status Updated Succesfullt'})
+            }
+        }
+        else{
+            return res.status(500).json(err)
+        }
+    })
+})
+
 module.exports = router
