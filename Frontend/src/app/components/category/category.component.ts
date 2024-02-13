6b09489667c85908923750f5d90cdd4a1b8ed4ba
satish <satishkonduru0225@gmail.com>
2024-02-13 07:46:59 +0530

    Category Completed

--- Frontend/src/app/components/category/category.component.ts
+++ Frontend/src/app/components/category/category.component.ts
@@ -1,10 +1,101 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { CategoryService } from 'src/app/services/category.service';
+import { SnackbarService } from 'src/app/services/snackbar.service';
+import { globalProperties } from 'src/app/shared/globalProperties';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.css']
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnInit{
+
+  categoryForm: any  = FormGroup
+  onAddCategory = new EventEmitter()
+  onEditCategory = new EventEmitter()
+  dialogAction : any = "Add"
+  action : any = "Add"
+  responseMsg: any;
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public dialogData ,
+    private _formBuilder: FormBuilder,
+    private _categoryService: CategoryService,
+    private _dialogRef: MatDialogRef<CategoryComponent>,
+    private _snackbar: SnackbarService
+  ){}
+
+  ngOnInit(): void {
+    this.categoryForm = this._formBuilder.group({
+      name: [null, [Validators.required]]
+    })
+    if(this.dialogData.action == 'Edit'){
+      this.dialogAction = 'Edit'
+      this.action = 'Update'
+      this.categoryForm.patchValue(this.dialogData.data)
+    }
+
+  }
+
+  handleSubmit(){
+
+    if(this.dialogAction == 'Edit'){
+      this.edit()
+    }
+    else{
+      this.add()
+    }
+
+  }
+
+  edit(){
+    var formData = this.categoryForm.value
+    var data = {
+      id: this.dialogData.data.id,
+      name: formData.name
+    }
+    this._categoryService.update(data)
+    .subscribe((res: any) => {
+      this._dialogRef.close()
+      this.onEditCategory.emit()
+      this.responseMsg = res?.message
+      this._snackbar.openSnackbar(this.responseMsg, 'success')
+    },
+    (err: any) => {
+      this._dialogRef.close()
+      if(err.error?.message){
+        this.responseMsg = err.error?.message
+      }
+      else{
+        this.responseMsg = globalProperties.genericError
+      }
+      this._snackbar.openSnackbar(this.responseMsg, globalProperties.error)
+    })
+  }
+  add(){
+    var formData = this.categoryForm.value
+    var data = {
+      name: formData.name
+    }
+    this._categoryService.add(data)
+    .subscribe((res: any) => {
+      this._dialogRef.close()
+      this.onAddCategory.emit()
+      this.responseMsg = res?.message
+      this._snackbar.openSnackbar(this.responseMsg, 'success')
+    },
+    (err: any) => {
+      this._dialogRef.close()
+      if(err.error?.message){
+        this.responseMsg = err.error?.message
+      }
+      else{
+        this.responseMsg = globalProperties.genericError
+      }
+      this._snackbar.openSnackbar(this.responseMsg, globalProperties.error)
+    })
+  }
+
 
 }
--- Frontend/src/app/components/manage-category/manage-category.component.ts
+++ Frontend/src/app/components/manage-category/manage-category.component.ts
@@ -66,9 +66,9 @@ this._router.events.subscribe( () => {
   dialogRef.close()
 })
 
-// dialogRef.componentInstance.onAddCategory.subscribe((res) => {
-//   this.tableData()
-// })
+dialogRef.componentInstance.onAddCategory.subscribe((res) => {
+  this.tableData()
+})
 
 }
 applyFilter(filterValue: string){
@@ -79,6 +79,28 @@ onSearchClear(){
   this.searchKey = ''
   this.applyFilter('')
 }
+editCategory(item: any){
+  const dialogConfig = new MatDialogConfig()
+  dialogConfig.data = {
+    action: 'Edit',
+    data: item
+  }
 
+  dialogConfig.width = '300px'
+  dialogConfig.disableClose = true
+  dialogConfig.position = {
+    top: '100px',
+    left: '48rem'
+  }
+  const dialogRef  = this._userDialog.open(CategoryComponent, dialogConfig)
+  this._router.events.subscribe(() => {
+    dialogRef.close()
+  })
+  dialogRef.componentInstance.onEditCategory.subscribe(() => {
+    this.tableData()
+  })
+
+
+}
 
 }
